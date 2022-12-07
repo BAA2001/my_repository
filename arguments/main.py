185465ba8e68bd9156468ebf183c1ae4c14c2ea0
Bilal <abouallalb@gmail.com>
2022-12-07 12:58:18 +0100

    Update main

--- arguments/main.py
+++ arguments/main.py
@@ -5,40 +5,41 @@ __human_name__ = "arguments"
 # Add your code after this line
 
 
-def greet(name, greeting_template="Hello, name!"):
-    x = greeting_template.replace("name", name)
-    print(x)
+def greet(name, greeting_template="Hello, <name>!"):
+    new_greeting = greeting_template.replace("<name>", name)
+    return new_greeting
 
 
 greet("Bilal")
-greet("Bilal", "Whats up, name!")
+greet("Bilal", "Whats up, <name>!")
 
 
 def force(mass, body="earth"):
     body = body.lower()
     bodies = {
         "sun": 274,
-        "jupiter": 24.9,
-        "neptune": 11.1,
-        "saturn": 10.4,
-        "earth": 9.8,
-        "uranus": 8.9,
-        "venus": 8.9,
-        "mars": 3.7,
+        "jupiter": 24.92,
+        "neptune": 11.15,
+        "saturn": 10.44,
+        "earth": 9.798,
+        "uranus": 8.87,
+        "venus": 8.87,
+        "mars": 3.71,
         "mercury": 3.7,
-        "moon": 1.6,
-        "pluto": 0.6,
+        "moon": 1.62,
+        "pluto": 0.58,
     }
-    print(mass * bodies.get(body))
+    generate_force = mass * round(bodies.get(body), 1)  # type: ignore
+    return generate_force
 
 
 force(5, "Sun")
 
 
 def pull(m1, m2, d):
-    g = 9.8
-    x = g * ((m1 * m2) / d**2)
-    print(x)
+    g = 6.674 * 10**-11
+    generate_pull = g * ((m1 * m2) / d**2)
+    return generate_pull
 
 
 pull(17.2, 5.5, 2.3)
--- strings/main.py
+++ strings/main.py
@@ -1,27 +1,27 @@
 # Do not modify these lines
-__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
-__human_name__ = 'strings'
+__winc_id__ = "71dd124b4a6e4d268f5973db521394ee"
+__human_name__ = "strings"
 
 # Add your code after this line
-scorer_0 = 'Ruud Gullit'
-scorer_1 = 'Marco van Basten'
+scorer_0 = "Ruud Gullit"
+scorer_1 = "Marco van Basten"
 
 goal_0 = 32
 goal_1 = 54
 
-scorers = scorer_0 + ' ' + str(goal_0), scorer_1 + ' ' + str(goal_1)
+scorers = scorer_0 + " " + str(goal_0), scorer_1 + " " + str(goal_1)
 
-report = f'{scorer_0} scored in the {goal_0}nd minute \n{scorer_1} scored in the {goal_1}th minute'
+report = f"{scorer_0} scored in the {goal_0}nd minute \n{scorer_1} scored in the {goal_1}th minute"
 
-player = 'Igor Belanov'
-first_name = player[:player.find(' ')]
-last_name = player[player.find(' '):]
+player = "Igor Belanov"
+first_name = player[: player.find(" ")]
+last_name = player[player.find(" ") :]
 
-last_name = last_name.replace(' ', '')
+last_name = last_name.replace(" ", "")
 
 last_name_length = len(last_name)
 
-name_short = player[0] + '.' + ' ' + last_name
+name_short = player[0] + "." + " " + last_name
 
-chant = (first_name + '!' + ' ') * (len(first_name)-1) + (first_name + '!')
-good_chant = chant[-1] != ' '
+chant = (first_name + "!" + " ") * (len(first_name) - 1) + (first_name + "!")
+good_chant = chant[-1] != " "
