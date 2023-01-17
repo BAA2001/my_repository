26b1c9ddfb2c80d4f3ad8638f78bb2b644730116
Bilal <abouallalb@gmail.com>
2023-01-17 16:02:00 +0100

    commit	modified:   ORM/betsy-webshop/betsy.db commit	modified:   ORM/betsy-webshop/main.py

--- ORM/betsy-webshop/main.py
+++ ORM/betsy-webshop/main.py
@@ -53,20 +53,20 @@ def remove_product(product_name):
     db.commit()
 
 
-def test_queries():
+def populate_test_database():
     # Create example data
-    user1 = User.create(name="John Doe", address="123 Main St", billing_info="Visa 200")
-    user2 = User.create(name="Jane Smith", address="456 Park Ave", billing_info="MC 56")
+    user1 = User.create(name="Bilal", address="123 street", billing_info="Visa 200")
+    user2 = User.create(name="Rachid", address="456 street", billing_info="MC 56")
     product1 = Product.create(
-        name="Handmade sweater",
-        description="Warm and cozy sweater made from 100% wool.",
+        name="Jacket",
+        description="Warm and cozy.",
         price=50.00,
         quantity=5,
         owner=user1,
     )
     product2 = Product.create(
-        name="Handmade scarf",
-        description="Soft and warm scarf made from 100% alpaca.",
+        name="Scarf",
+        description="Soft and warm.",
         price=30.00,
         quantity=3,
         owner=user2,
@@ -78,38 +78,38 @@ def test_queries():
     ProductTag.create(product=product2, tag=tag1)
 
     # Test search products query
-    results = search("sweater")
+    results = search("Jacket")
     assert len(results) == 1
-    assert results[0].name == "Handmade sweater"
+    assert results[0].name == "Jacket"
 
     # Test list user products query
-    results = list_user_products("John Doe")
+    results = list_user_products("Bilal")
     assert len(results) == 1
-    assert results[0].name == "Handmade sweater"
+    assert results[0].name == "Jacket"
 
     # Test list products per tag query
     results = list_products_per_tag("Winter")
     assert len(results) == 1
-    assert results[0].name == "Handmade sweater"
+    assert results[0].name == "Jacket"
 
     # Test add product to catalog query
-    add_product_to_catalog("Handmade scarf", "John Doe")
-    results = list_user_products("John Doe")
+    add_product_to_catalog("Scarf", "Bilal")
+    results = list_user_products("Bilal")
     assert len(results) == 2
-    assert results[1].name == "Handmade scarf"
+    assert results[1].name == "Scarf"
 
     # Test update stock query
-    update_stock("Handmade sweater", 10)
-    results = Product.get(Product.name == "Handmade sweater")
+    update_stock("Jacket", 10)
+    results = Product.get(Product.name == "Jacket")
     assert results.quantity == 10
 
     # Test purchase product query
-    purchase_product("Handmade sweater", "Jane Smith", 2)
-    results = Product.get(Product.name == "Handmade sweater")
+    purchase_product("Jacket", "Rachid", 2)
+    results = Product.get(Product.name == "Jacket")
     assert results.quantity == 8
     results = Transaction.select().where(Transaction.product == product1)
     assert len(results) == 1
     assert results[0].quantity
 
 
-test_queries()
+populate_test_database()
